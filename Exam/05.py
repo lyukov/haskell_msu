19b5f6fa404c52b70c1d612a0d87b2405bcca20c
Dmitry Lyukov <d@lyukov.com>
2021-04-21 01:52:19 +0300

    Add 05.py

---
+++ Exam/05.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+
+opening = {
+    ')' : '(',
+    ']' : '[',
+    '}' : '{',
+    '>' : '<'
+}
+opens = opening.values()
+closes = opening.keys()
+
+def max_nesting(sequence):
+    stack = []
+    maxNesting = 0
+    for s in sequence:
+        if s in opens:
+            stack.append(s)
+            maxNesting = max(maxNesting, len(stack))
+        elif s in closes:
+            if len(stack) == 0: return -1
+            if opening[s] != stack.pop(): return -1
+        else:
+            return -1
+    return maxNesting
+
+if __name__ == '__main__':
+    print(max_nesting(input()))
